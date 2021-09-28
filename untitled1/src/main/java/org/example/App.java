b844f1b112dfac80a153070ae0270054f4672932
g <g>
2021-09-28 10:56:38 -0400

    Ecrire fichier

--- untitled1/src/main/java/org/example/App.java
+++ untitled1/src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,10 +13,40 @@ public class App
 {
    static String[] objList=new String[]{"Bonjour","Monsieur","Monsieur","Ca","Va",};
     public static void main( String[] args ) throws Exception {
-       Trouveru testTrouve = new Trouveru(objList,"Csa");
-       int resultat = 0;
-       resultat = testTrouve.TrouverIndex();
+        //Trouverr("Cax");
+        EcrireFichier();
+
+    }
+    public static void Trouverr(String motCherche) throws Exception {
+
+        Trouveru testTrouve = new Trouveru(objList,motCherche);
+        int resultat = 0;
+        resultat = testTrouve.TrouverIndex();
         System.out.println(resultat);
+    }
+
+    public static void EcrireFichier()
+    {
+        String path="";
+        path+= System.getProperty("user.dir");
+        //path = path.replace("\\", "\\\\");
+        System.out.println(path+"\\hello.txt");
+
+        try {
+           // File myObj = new File(path+"\\Hello.txt");
+            File myObj = new File(path,"GhilesKouaou.txt");
+            System.out.println(myObj.getParent());
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        ;
+
     }
 
 
